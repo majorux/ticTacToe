c232f4585f9ab4c952b2ea6d7e3a83d98dc49422
majorux <farabd90@gmail.com>
2023-03-03 09:32:55 +0100

    add function for selecting X or O for player 1 and player 2. add function for creating squares

---
+++ scrypt.js
@@ -0,0 +1,68 @@
+const DOM = (() =>{
+    return {
+        selection: document.querySelectorAll('.seletion'),
+
+        makeActive: function (e) {
+            e.target.parentNode.querySelectorAll('.selection').foreach((child) => {
+                child.classList.remove('active');
+            });
+            e.target.classList.add('.active');
+            if(e.target.classList.contains('human')){
+                if(e.target.classList.contains('one')){
+                    Controller.player1.type = 'human';
+                } else {
+                    Controller.player2.type = 'human';
+                }
+            } else if(e.target.classList.contains('one')){
+                Controller.player1.type = 'ai'
+            } else {
+                Controller.player2.type = 'ai'
+            }
+        },
+
+        boardContainer: document.querySelector('.gameboard'),
+        //fetch current squares
+        getSquares: function () {
+            return this.boardContainer.querySelectorAll('.square');
+        },
+
+        newSquare: function (html){
+            const square = document.createElement('div');
+            square.className = 'square';
+            square.innerHTML = html
+            return square;
+        },
+
+        newSquareInner: function(mark){
+            return `<span>${mark}</span>`;
+        },
+
+        render: function (board) {
+            this.clearBoard();
+            board.forEach((square) => {
+                this.boardContainer.appendChild(
+                    DOM.newSquare(DOM.newSquareInner(square.mark))
+                )
+            });
+        },
+
+        setupWindow: document.querySelector('.setup'),
+        startButton: document.querySelector('.startgame'),
+        turnIndicator: document.querySelector('.turn'),
+        winnerBanner: document.querySelector('.winner'),
+
+        winDisplay: function(winner){
+            if(winner){
+                this.winnerBanner.textContent = `${winner} wins!` ;
+            } else {
+                this.winnerBanner.textContent = "It's a tie!";
+            }
+            const playAgainContainer = document.createElement('div');
+            playAgainContainer.className = 'playagaincontainer';
+            const playAgain = document.createElement('button');
+            
+        }
+
+        
+    }
+})
